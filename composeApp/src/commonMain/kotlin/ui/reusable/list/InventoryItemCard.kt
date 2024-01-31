ca347cab181d6b276c01f706fe861bd8916a78b6
Arnau Mora <arnyminerz@proton.me>
2024-01-31 09:50:46 +0100

    Added default icon
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/ui/reusable/list/InventoryItemCard.kt
+++ composeApp/src/commonMain/kotlin/ui/reusable/list/InventoryItemCard.kt
@@ -1,12 +1,23 @@
 package ui.reusable.list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Category
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import backend.database.InventoryItem
 import com.eygraber.compose.placeholder.PlaceholderHighlight
@@ -21,19 +32,33 @@ import kotlin.random.Random
 @Composable
 fun InventoryItemCard(item: InventoryItem?, modifier: Modifier = Modifier) {
     OutlinedCard(
-        modifier = Modifier.width(200.dp).height(150.dp).then(modifier)
+        modifier = Modifier.width(200.dp).then(modifier)
     ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .padding(8.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(MaterialTheme.colorScheme.surfaceVariant)
+                .placeholder(visible = item == null, highlight = PlaceholderHighlight.fade()),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(Icons.Rounded.Category, null)
+        }
         Text(
             text = item?.displayName ?: "X".repeat(Random.Default.nextInt(5, 10)),
             modifier = Modifier
-                .padding(8.dp)
-                .placeholder(visible = item == null, highlight = PlaceholderHighlight.fade())
+                .padding(horizontal = 8.dp)
+                .placeholder(visible = item == null, highlight = PlaceholderHighlight.fade()),
+            style = MaterialTheme.typography.labelLarge
         )
         Text(
             text = "#${item?.id ?: 10} - ${item?.let { it.category ?: "null" } ?: 10}",
             modifier = Modifier
                 .padding(horizontal = 8.dp).padding(bottom = 8.dp)
-                .placeholder(visible = item == null, highlight = PlaceholderHighlight.fade())
+                .placeholder(visible = item == null, highlight = PlaceholderHighlight.fade()),
+            style = MaterialTheme.typography.labelSmall
         )
     }
 }
