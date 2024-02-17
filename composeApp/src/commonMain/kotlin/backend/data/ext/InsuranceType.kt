5b00564ee4b7263f4135b4484c875b772bde2778
Arnau Mora <arnyminerz@proton.me>
2024-02-17 10:42:27 +0100

    Import strings
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/backend/data/ext/InsuranceType.kt
+++ composeApp/src/commonMain/kotlin/backend/data/ext/InsuranceType.kt
@@ -1,6 +1,9 @@
 package backend.data.ext
 
 import app.composeapp.generated.resources.Res
+import app.composeapp.generated.resources.lending_auth_insurance_federative_femecv
+import app.composeapp.generated.resources.lending_auth_insurance_federative_other
+import app.composeapp.generated.resources.lending_auth_insurance_private
 import org.jetbrains.compose.resources.StringResource
 
 enum class InsuranceType(val labelRes: StringResource) {
