b2718dbe11aa5a7cce94bfd3b2223dd3eb04f616
Arnau Mora Gras <arnyminerz@proton.me>
2024-11-14 14:46:44 +0100

    Added app icon
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/desktopMain/kotlin/org/centrexcursionistalcoi/app/main.kt
+++ composeApp/src/desktopMain/kotlin/org/centrexcursionistalcoi/app/main.kt
@@ -2,8 +2,11 @@ package org.centrexcursionistalcoi.app
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import ceaapp.composeapp.generated.resources.CEA
+import ceaapp.composeapp.generated.resources.Res
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
+import org.jetbrains.compose.resources.painterResource
 
 fun main() {
     Napier.base(DebugAntilog())
@@ -11,6 +14,7 @@ fun main() {
     application {
         Window(
             onCloseRequest = ::exitApplication,
+            icon = painterResource(Res.drawable.CEA),
             title = "CEA App",
         ) {
             AppRoot()
