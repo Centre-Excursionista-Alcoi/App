6f1bd6dfe7062c1f66897e129f1e26bb5247adda
Arnau Mora Gras <arnyminerz@proton.me>
2024-11-14 18:16:35 +0100

    Got rid of Docker compiler
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- entrypoint/entrypoint.bat
+++
@@ -1,3 +0,0 @@
-& gradlew.bat --no-daemon :composeApp:packageDistributionForCurrentOS
-
-cp C:\\source\\composeApp\\build\\compose\\binaries\\main\\* C:\\output
--- entrypoint/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-./gradlew --no-daemon :composeApp:packageDistributionForCurrentOS || cat /source/composeApp/build/compose/logs/createRuntimeImage/*-err.txt
-
-cp /source/composeApp/build/compose/binaries/main/* /output
