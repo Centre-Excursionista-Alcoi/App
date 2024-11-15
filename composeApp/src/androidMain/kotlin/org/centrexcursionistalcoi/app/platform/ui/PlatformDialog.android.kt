be9b56a136cbcbbf376b2665017788f0100524ca
Arnau Mora Gras <arnyminerz@proton.me>
2024-11-15 12:09:37 +0100

    Added background to dialog
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformDialog.android.kt
+++ composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformDialog.android.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.DialogProperties
@@ -19,6 +20,10 @@ actual fun PlatformDialog(
     BasicAlertDialog(
         onDismissRequest = onDismissRequest
     ) {
-        Column(modifier = Modifier.fillMaxWidth()) { content() }
+        Surface(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(modifier = Modifier.fillMaxWidth()) { content() }
+        }
     }
 }
