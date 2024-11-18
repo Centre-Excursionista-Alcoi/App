29795bfe471a1b63229195568af547854ed45427
Arnau Mora Gras <arnyminerz@proton.me>
2024-11-18 13:12:04 +0100

    Added db timeout
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- server/src/main/kotlin/org/centrexcursionistalcoi/app/Application.kt
+++ server/src/main/kotlin/org/centrexcursionistalcoi/app/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 import org.centrexcursionistalcoi.app.database.ServerDatabase
 import org.centrexcursionistalcoi.app.plugins.configureRouting
 import org.centrexcursionistalcoi.app.plugins.installAuthentication
@@ -16,12 +17,14 @@ fun main() = runBlocking {
 }
 
 private suspend fun start() {
-    val databaseUrl = System.getenv("DATABASE_URL") ?: "jdbc:h2:file:./CEA"
-    val databaseDriver = System.getenv("DATABASE_DRIVER") ?: "org.h2.Driver"
-    val databaseUsername = System.getenv("DATABASE_USERNAME") ?: ""
-    val databasePassword = System.getenv("DATABASE_PASSWORD") ?: ""
+    withTimeout(10_000) {
+        val databaseUrl = System.getenv("DATABASE_URL") ?: "jdbc:h2:file:./CEA"
+        val databaseDriver = System.getenv("DATABASE_DRIVER") ?: "org.h2.Driver"
+        val databaseUsername = System.getenv("DATABASE_USERNAME") ?: ""
+        val databasePassword = System.getenv("DATABASE_PASSWORD") ?: ""
 
-    ServerDatabase.initialize(databaseUrl, databaseDriver, databaseUsername, databasePassword)
+        ServerDatabase.initialize(databaseUrl, databaseDriver, databaseUsername, databasePassword)
+    }
 
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
