32e00904a70f8baaf1d9125ace9eedd1da36ed61
Arnau Mora Gras <arnyminerz@proton.me>
2024-11-18 12:35:32 +0100

    Added status pages
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- server/src/main/kotlin/org/centrexcursionistalcoi/app/plugins/StatusPages.kt
+++ server/src/main/kotlin/org/centrexcursionistalcoi/app/plugins/StatusPages.kt
@@ -2,12 +2,16 @@ package org.centrexcursionistalcoi.app.plugins
 
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.install
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.respondText
 
 fun Application.installStatusPages() {
     install(StatusPages) {
+        exception { call: ApplicationCall, cause: Exception ->
+            call.respondText(text = "500: Internal Server Error.\n$cause", status = HttpStatusCode.InternalServerError)
+        }
         status(HttpStatusCode.NotFound) { call, status ->
             call.respondText(text = "404: Page Not Found", status = status)
         }
