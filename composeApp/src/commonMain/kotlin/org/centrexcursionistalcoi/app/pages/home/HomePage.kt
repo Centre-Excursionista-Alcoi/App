708f111a56bd7a10a60a68e923a05c1241b892af
Arnau Mora Gras <arnyminerz@proton.me>
2024-11-19 08:02:04 +0100

    Hide headers of empty sections
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/pages/home/HomePage.kt
+++ composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/pages/home/HomePage.kt
@@ -32,73 +32,93 @@ fun HomePage(bookings: List<LendingD>?) {
             val incompleteBookings = bookings.filter { it.returnedAt == null }
             val completeBookings = bookings.filter { it.returnedAt != null }
 
-            item(
-                key = "incomplete-header",
-                span = maxGridItemSpan
-            ) {
-                BasicText(
-                    text = stringResource(Res.string.bookings_your),
-                    modifier = Modifier.fillMaxWidth(),
-                    style = getPlatformTextStyles().titleRegular.copy(fontSize = 20.sp)
-                )
-            }
-            items(incompleteBookings) { booking ->
-                PlatformCard(
-                    title = stringResource(Res.string.bookings_items_count, booking.itemIds?.size ?: 0),
-                    modifier = Modifier.padding(8.dp)
+            if (incompleteBookings.isEmpty() && completeBookings.isEmpty()) {
+                item(
+                    key = "no-bookings",
+                    span = maxGridItemSpan
                 ) {
-                    if (!booking.confirmed) {
+                    BasicText(
+                        text = stringResource(Res.string.bookings_no_bookings),
+                        modifier = Modifier.fillMaxWidth(),
+                        style = getPlatformTextStyles().titleRegular.copy(fontSize = 20.sp)
+                    )
+                }
+                return@LazyVerticalGrid
+            }
+
+            if (incompleteBookings.isNotEmpty()) {
+                item(
+                    key = "incomplete-header",
+                    span = maxGridItemSpan
+                ) {
+                    BasicText(
+                        text = stringResource(Res.string.bookings_your),
+                        modifier = Modifier.fillMaxWidth(),
+                        style = getPlatformTextStyles().titleRegular.copy(fontSize = 20.sp)
+                    )
+                }
+                items(incompleteBookings) { booking ->
+                    PlatformCard(
+                        title = stringResource(Res.string.bookings_items_count, booking.itemIds?.size ?: 0),
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        if (!booking.confirmed) {
+                            BasicText(
+                                text = stringResource(Res.string.bookings_pending_confirmation),
+                                modifier = Modifier.padding(8.dp)
+                            )
+                        } else if (booking.takenAt == null) {
+                            BasicText(
+                                text = stringResource(Res.string.bookings_not_taken),
+                                modifier = Modifier.padding(8.dp)
+                            )
+                        } else if (booking.takenAt != null && booking.returnedAt == null) {
+                            BasicText(
+                                text = stringResource(Res.string.bookings_pending_return),
+                                modifier = Modifier.padding(8.dp)
+                            )
+                        }
                         BasicText(
-                            text = stringResource(Res.string.bookings_pending_confirmation),
-                            modifier = Modifier.padding(8.dp)
+                            text = stringResource(Res.string.bookings_from, booking.fromDate().toString()),
+                            modifier = Modifier.padding(horizontal = 8.dp).padding(top = 8.dp)
                         )
-                    } else if (booking.takenAt == null) {
                         BasicText(
-                            text = stringResource(Res.string.bookings_not_taken),
-                            modifier = Modifier.padding(8.dp)
-                        )
-                    } else if (booking.takenAt != null && booking.returnedAt == null) {
-                        BasicText(
-                            text = stringResource(Res.string.bookings_pending_return),
-                            modifier = Modifier.padding(8.dp)
+                            text = stringResource(Res.string.bookings_to, booking.toDate().toString()),
+                            modifier = Modifier.padding(horizontal = 8.dp).padding(bottom = 8.dp)
                         )
                     }
-                    BasicText(
-                        text = stringResource(Res.string.bookings_from, booking.fromDate().toString()),
-                        modifier = Modifier.padding(horizontal = 8.dp).padding(top = 8.dp)
-                    )
-                    BasicText(
-                        text = stringResource(Res.string.bookings_to, booking.toDate().toString()),
-                        modifier = Modifier.padding(horizontal = 8.dp).padding(bottom = 8.dp)
-                    )
+                }
+
+                if (completeBookings.isNotEmpty()) {
+                    item(key = "middle-space", contentType = "spacer") { Spacer(Modifier.height(16.dp)) }
                 }
             }
 
-            item(key = "middle-space", contentType = "spacer") { Spacer(Modifier.height(16.dp)) }
-
-            item(
-                key = "old-header",
-                span = maxGridItemSpan
-            ) {
-                BasicText(
-                    text = stringResource(Res.string.bookings_old),
-                    modifier = Modifier.fillMaxWidth(),
-                    style = getPlatformTextStyles().titleRegular.copy(fontSize = 20.sp)
-                )
-            }
-            items(completeBookings) { booking ->
-                PlatformCard(
-                    title = stringResource(Res.string.bookings_items_count, booking.itemIds?.size ?: 0),
-                    modifier = Modifier.padding(8.dp)
+            if (completeBookings.isNotEmpty()) {
+                item(
+                    key = "old-header",
+                    span = maxGridItemSpan
                 ) {
                     BasicText(
-                        text = stringResource(Res.string.bookings_from, booking.fromDate().toString()),
-                        modifier = Modifier.padding(horizontal = 8.dp)
+                        text = stringResource(Res.string.bookings_old),
+                        modifier = Modifier.fillMaxWidth(),
+                        style = getPlatformTextStyles().titleRegular.copy(fontSize = 20.sp)
                     )
-                    BasicText(
-                        text = stringResource(Res.string.bookings_to, booking.toDate().toString()),
-                        modifier = Modifier.padding(horizontal = 8.dp).padding(bottom = 8.dp)
-                    )
+                }
+                items(completeBookings) { booking ->
+                    PlatformCard(
+                        title = stringResource(Res.string.bookings_items_count, booking.itemIds?.size ?: 0),
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        BasicText(
+                            text = stringResource(Res.string.bookings_from, booking.fromDate().toString()),
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        )
+                        BasicText(
+                            text = stringResource(Res.string.bookings_to, booking.toDate().toString()),
+                            modifier = Modifier.padding(horizontal = 8.dp).padding(bottom = 8.dp)
+                        )
+                    }
                 }
             }
         }
