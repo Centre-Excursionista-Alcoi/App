745585843a23d268014e4b8bed8a4c5fc129c3f2
Arnau Mora Gras <arnyminerz@proton.me>
2024-11-19 14:57:57 +0100

    Updated taken checks
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- server/src/main/kotlin/org/centrexcursionistalcoi/app/database/utils/Lendings.kt
+++ server/src/main/kotlin/org/centrexcursionistalcoi/app/database/utils/Lendings.kt
@@ -2,35 +2,40 @@ package org.centrexcursionistalcoi.app.database.utils
 
 import java.time.LocalDate
 import kotlin.io.encoding.ExperimentalEncodingApi
+import org.centrexcursionistalcoi.app.data.IBookingD
 import org.centrexcursionistalcoi.app.data.ItemD
-import org.centrexcursionistalcoi.app.data.ItemLendingD
-import org.centrexcursionistalcoi.app.data.SpaceBookingD
 import org.centrexcursionistalcoi.app.data.SpaceD
+import org.centrexcursionistalcoi.app.database.common.BookingEntity
+import org.centrexcursionistalcoi.app.database.common.BookingTable
 import org.centrexcursionistalcoi.app.database.entity.Item
-import org.centrexcursionistalcoi.app.database.entity.Lending
 import org.centrexcursionistalcoi.app.database.entity.LendingItem
 import org.centrexcursionistalcoi.app.database.entity.Space
 import org.centrexcursionistalcoi.app.database.entity.SpaceBooking
 import org.centrexcursionistalcoi.app.database.table.LendingItemsTable
-import org.centrexcursionistalcoi.app.database.table.LendingsTable
 import org.centrexcursionistalcoi.app.database.table.SpaceBookingsTable
+import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.sql.and
 
 /**
- * Fetch all item lendings that overlap with the given period.
+ * Fetch all lendings that overlap with the given period.
  *
  * If they partially overlap, they are considered to overlap.
  *
+ * Filters automatically already returned bookings.
+ *
  * **Must be called within a transaction.**
  */
-fun itemLendingsForDates(from: LocalDate, to: LocalDate): List<ItemLendingD> {
-    return Lending.find {
+fun <BookingD : IBookingD, Entity : BookingEntity<BookingD>, EntityClass : IntEntityClass<Entity>, Table : BookingTable> lendingsForDates(
+    from: LocalDate,
+    to: LocalDate,
+    entityClass: EntityClass,
+    table: Table
+): List<BookingD> {
+    return entityClass.find {
         // (StartA <= EndB) and (EndA >= StartB)
         // Proof: https://stackoverflow.com/a/325964
-        (LendingsTable.from lessEq to) and (LendingsTable.to greaterEq from) and
-            // Only fetch the lendings that are not returned
-            (LendingsTable.returnedAt eq null)
-    }.map(Lending::serializable)
+        (table.from lessEq to) and (table.to greaterEq from) and (table.returnedAt.isNull())
+    }.map { it.serializable() }
 }
 
 /**
@@ -42,32 +47,12 @@ fun itemsAvailableForDates(from: LocalDate, to: LocalDate): List<ItemD> {
     // Fetch all items in the database
     val allItems = Item.all().map(Item::serializable)
     // Fetch the existing lendings that overlap with the requested period
-    val lendingsIds = itemLendingsForDates(from, to).mapNotNull { it.id }
+    val lendingsIds = lendingsForDates(from, to, SpaceBooking, SpaceBookingsTable).mapNotNull { it.id }
     // Fetch all the items booked for the requested period
     val usedItemsIds = LendingItem.find { LendingItemsTable.lending inList lendingsIds }
         .map { it.item.id.value }
     // Return all the items in the database that are not used
     return allItems.filter { it.id !in usedItemsIds }
-}
-
-
-/**
- * Fetch all space lendings that overlap with the given period.
- *
- * If they partially overlap, they are considered to overlap.
- *
- * Filters automatically already paid bookings.
- *
- * **Must be called within a transaction.**
- */
-fun spaceLendingsForDates(from: LocalDate, to: LocalDate): List<SpaceBookingD> {
-    return SpaceBooking.find {
-        // (StartA <= EndB) and (EndA >= StartB)
-        // Proof: https://stackoverflow.com/a/325964
-        (SpaceBookingsTable.from lessEq to) and (SpaceBookingsTable.to greaterEq from) and
-            // Only fetch the lendings that are not returned
-            (SpaceBookingsTable.paid eq false)
-    }.map(SpaceBooking::serializable)
 }
 
 /**
@@ -80,7 +65,7 @@ fun spacesAvailableForDates(from: LocalDate, to: LocalDate): List<SpaceD> {
     // Fetch all spaces in the database
     val allSpaces = Space.all().map(Space::serializable)
     // Fetch the existing lendings that overlap with the requested period
-    val bookingsIds = spaceLendingsForDates(from, to).mapNotNull { it.id }
+    val bookingsIds = lendingsForDates(from, to, SpaceBooking, SpaceBookingsTable).mapNotNull { it.id }
     // Fetch all the spaces booked for the requested period
     val usedSpacesIds = SpaceBooking.find { SpaceBookingsTable.space inList bookingsIds }
         .map { it.space.id.value }
