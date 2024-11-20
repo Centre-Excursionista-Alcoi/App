aedf6c740631656b7b51e0dbdaf438e0aba2f185
Arnau Mora Gras <arnyminerz@proton.me>
2024-11-20 19:12:08 +0100

    Fixed dark theme text color
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformTextStyle.android.kt
+++ composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformTextStyle.android.kt
@@ -1,15 +1,17 @@
 package org.centrexcursionistalcoi.app.platform.ui
 
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 
 @Composable
 actual fun getPlatformTextStyles(): PlatformTextStyles {
+    val contentColor = LocalContentColor.current
     return PlatformTextStyles(
-        titleLarge = MaterialTheme.typography.headlineLarge,
-        titleRegular = MaterialTheme.typography.headlineSmall,
-        heading = MaterialTheme.typography.titleSmall,
-        body = MaterialTheme.typography.bodyMedium,
-        label = MaterialTheme.typography.labelMedium
+        titleLarge = MaterialTheme.typography.headlineLarge.copy(color = contentColor),
+        titleRegular = MaterialTheme.typography.headlineSmall.copy(color = contentColor),
+        heading = MaterialTheme.typography.titleSmall.copy(color = contentColor),
+        body = MaterialTheme.typography.bodyMedium.copy(color = contentColor),
+        label = MaterialTheme.typography.labelMedium.copy(color = contentColor)
     )
 }
