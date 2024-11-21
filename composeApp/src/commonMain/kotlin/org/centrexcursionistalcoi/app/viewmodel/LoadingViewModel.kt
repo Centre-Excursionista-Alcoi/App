203f7b1dc11b26c94e2c684726e0afc8547926fe
Arnau Mora Gras <arnyminerz@proton.me>
2024-11-21 09:56:04 +0100

    Got rid of topic subscription
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/viewmodel/LoadingViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/viewmodel/LoadingViewModel.kt
@@ -2,7 +2,6 @@ package org.centrexcursionistalcoi.app.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
-import com.mmk.kmpnotifier.notification.NotifierManager
 import io.github.aakira.napier.Napier
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -10,7 +9,6 @@ import org.centrexcursionistalcoi.app.auth.AccountManager
 import org.centrexcursionistalcoi.app.network.AuthBackend
 import org.centrexcursionistalcoi.app.network.Backend
 import org.centrexcursionistalcoi.app.network.Sync
-import org.centrexcursionistalcoi.app.push.PushTopic
 import org.centrexcursionistalcoi.app.route.Home
 import org.centrexcursionistalcoi.app.route.Login
 
@@ -39,11 +37,6 @@ class LoadingViewModel : ViewModel() {
                 if (account != null) {
                     // Login again to refresh the token
                     AuthBackend.login(account.first.email, account.second)
-
-                    // Subscribe to the user's topic
-                    val topic = PushTopic.topic(account.first.email)
-                    Napier.i { "Subscribing to topic for FCM notifications: $topic" }
-                    NotifierManager.getPushNotifier().subscribeToTopic(topic)
 
                     Sync.syncBasics()
 
