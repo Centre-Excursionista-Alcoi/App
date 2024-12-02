d2021bb995eac2a3cfcc860c6cdb893e3a8aeb8e
Arnau Mora Gras <arnyminerz@proton.me>
2024-12-02 12:14:43 +0100

    Implemented notification endpoints
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- server/src/main/kotlin/org/centrexcursionistalcoi/app/endpoints/notifications/MarkNotificationAsViewedEndpoint.kt
+++ server/src/main/kotlin/org/centrexcursionistalcoi/app/endpoints/notifications/MarkNotificationAsViewedEndpoint.kt
@@ -2,11 +2,27 @@ package org.centrexcursionistalcoi.app.endpoints.notifications
 
 import io.ktor.http.HttpMethod
 import io.ktor.server.routing.RoutingContext
+import org.centrexcursionistalcoi.app.database.ServerDatabase
 import org.centrexcursionistalcoi.app.database.entity.User
+import org.centrexcursionistalcoi.app.database.entity.notification.Notification
 import org.centrexcursionistalcoi.app.endpoints.model.SecureEndpoint
+import org.centrexcursionistalcoi.app.server.response.Errors
 
 object MarkNotificationAsViewedEndpoint : SecureEndpoint("/notifications/{id}/markAsViewed", HttpMethod.Post) {
     override suspend fun RoutingContext.secureBody(user: User) {
-        TODO("Not yet implemented")
+        val notificationId = call.parameters["id"]?.toIntOrNull()
+        val notification = notificationId?.let { id ->
+            ServerDatabase { Notification.findById(id) }
+        }
+        if (notification == null) {
+            respondFailure(Errors.ObjectNotFound)
+            return
+        }
+
+        ServerDatabase {
+            notification.viewed = true
+        }
+
+        respondSuccess()
     }
 }
