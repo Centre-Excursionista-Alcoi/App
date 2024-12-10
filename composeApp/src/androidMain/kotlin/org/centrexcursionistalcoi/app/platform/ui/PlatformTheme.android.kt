3e659a9d329ba10480c8fed15c5f4afa7e09f251
Arnau Mora Gras <arnyminerz@proton.me>
2024-12-10 13:54:30 +0100

    Fixed background image
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformTheme.android.kt
+++ composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformTheme.android.kt
@@ -9,11 +9,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import ceaapp.composeapp.generated.resources.*
 import org.centrexcursionistalcoi.app.R
 import org.centrexcursionistalcoi.app.ui.theme.AppTheme
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 actual fun PlatformTheme(content: @Composable () -> Unit) {
@@ -27,7 +26,7 @@ actual fun PlatformTheme(content: @Composable () -> Unit) {
             modifier = Modifier.fillMaxSize()
         ) {
             Image(
-                painter = painterResource(Res.drawable.CEA_Monochrome),
+                painter = painterResource(R.drawable.cea_monochrome),
                 contentDescription = stringResource(R.string.app_name),
                 modifier = if (isLandscape) {
                     Modifier.fillMaxHeight(.8f)
