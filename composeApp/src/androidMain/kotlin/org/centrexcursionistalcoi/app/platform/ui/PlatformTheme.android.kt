724049ccafbb90766de414b5cceb6086ab1b3e62
Arnau Mora Gras <arnyminerz@proton.me>
2024-12-10 11:35:28 +0100

    Added CEA logo to the background
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformTheme.android.kt
+++ composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformTheme.android.kt
@@ -1,11 +1,43 @@
 package org.centrexcursionistalcoi.app.platform.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import ceaapp.composeapp.generated.resources.*
+import org.centrexcursionistalcoi.app.R
 import org.centrexcursionistalcoi.app.ui.theme.AppTheme
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 actual fun PlatformTheme(content: @Composable () -> Unit) {
-    AppTheme(
-        content = content
-    )
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp
+    val screenWidth = configuration.screenWidthDp
+    val isLandscape = screenWidth > screenHeight
+
+    AppTheme {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(Res.drawable.CEA_Monochrome),
+                contentDescription = stringResource(R.string.app_name),
+                modifier = if (isLandscape) {
+                    Modifier.fillMaxHeight(.8f)
+                } else {
+                    Modifier.fillMaxWidth(.8f)
+                }
+                    .align(Alignment.Center),
+                alpha = .05f
+            )
+            content()
+        }
+    }
 }
--- composeApp/src/desktopMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformTheme.desktop.kt
+++ composeApp/src/desktopMain/kotlin/org/centrexcursionistalcoi/app/platform/ui/PlatformTheme.desktop.kt
@@ -1,11 +1,29 @@
 package org.centrexcursionistalcoi.app.platform.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import ceaapp.composeapp.generated.resources.*
 import com.gabrieldrn.carbon.CarbonDesignSystem
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 actual fun PlatformTheme(content: @Composable () -> Unit) {
     CarbonDesignSystem {
-        content()
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(Res.drawable.CEA_Monochrome),
+                contentDescription = "",
+                modifier = Modifier.fillMaxHeight(.4f).align(Alignment.Center),
+                alpha = .05f
+            )
+            content()
+        }
     }
 }
