8c303356366a3532711c5f7933528097e63ff1f8
Arnau Mora <arnyminerz@proton.me>
2025-10-02 13:09:26 +0200

    Add loading indicator

--- composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/AuthCallbackActivity.kt
+++ composeApp/src/androidMain/kotlin/org/centrexcursionistalcoi/app/AuthCallbackActivity.kt
@@ -3,12 +3,19 @@ package org.centrexcursionistalcoi.app
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import io.ktor.http.Url
 import kotlinx.coroutines.runBlocking
 import org.centrexcursionistalcoi.app.auth.AuthCallbackProcessor
+import org.centrexcursionistalcoi.app.ui.theme.AppTheme
 import org.centrexcursionistalcoi.app.viewmodel.AuthCallbackModel
 
 class AuthCallbackActivity: ComponentActivity() {
@@ -26,6 +33,14 @@ class AuthCallbackActivity: ComponentActivity() {
                     flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 }
             )
+        }
+
+        setContent {
+            AppTheme {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator()
+                }
+            }
         }
     }
 }
