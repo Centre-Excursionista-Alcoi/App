e4848b47e6f16e3d227862509c0da8e0da4b9bcf
Arnau Mora <arnyminerz@proton.me>
2025-10-24 16:30:37 +0200

    Added notifications
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/push/PushNotifierListener.kt
+++ composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/push/PushNotifierListener.kt
@@ -1,16 +1,36 @@
 package org.centrexcursionistalcoi.app.push
 
+import cea_app.composeapp.generated.resources.*
 import com.mmk.kmpnotifier.notification.NotifierManager
 import com.mmk.kmpnotifier.notification.PayloadData
 import io.github.aakira.napier.Napier
+import kotlin.random.Random
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.centrexcursionistalcoi.app.defaultAsyncDispatcher
 import org.centrexcursionistalcoi.app.sync.BackgroundJobCoordinator
 import org.centrexcursionistalcoi.app.sync.SyncLendingBackgroundJob
 import org.centrexcursionistalcoi.app.sync.SyncLendingBackgroundJobLogic
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.getString
 
 object PushNotifierListener : NotifierManager.Listener {
+    private fun showNotification(notificationTitleRes: StringResource, notificationBodyResource: StringResource, data: Map<String, String>) {
+        CoroutineScope(defaultAsyncDispatcher).launch {
+            val notifier = NotifierManager.getLocalNotifier()
+
+            val notificationTitle = getString(notificationTitleRes)
+            val notificationBody = getString(notificationBodyResource)
+
+            notifier.notify {
+                id = Random.nextInt()
+                title = notificationTitle
+                body = notificationBody
+                payloadData = data
+            }
+        }
+    }
+
     override fun onNewToken(token: String) {
         Napier.i("onNewToken: $token")
 
@@ -26,6 +46,25 @@ object PushNotifierListener : NotifierManager.Listener {
             val notification = PushNotification.fromData(data.mapValues { it.toString() })
             when (notification) {
                 is PushNotification.LendingConfirmed -> {
+                    BackgroundJobCoordinator.schedule<SyncLendingBackgroundJob>(
+                        input = mapOf(SyncLendingBackgroundJobLogic.EXTRA_LENDING_ID to notification.lendingId.toString()),
+                    )
+
+                    showNotification(
+                        Res.string.notification_lending_confirmed_title,
+                        Res.string.notification_lending_confirmed_message,
+                        data.mapValues { it.toString() }
+                    )
+                }
+
+                // --- Admin notifications ---
+                is PushNotification.NewLendingRequest -> {
+                    // TODO: Show notification
+                    BackgroundJobCoordinator.schedule<SyncLendingBackgroundJob>(
+                        input = mapOf(SyncLendingBackgroundJobLogic.EXTRA_LENDING_ID to notification.lendingId.toString()),
+                    )
+                }
+                is PushNotification.NewMemoryUpload -> {
                     // TODO: Show notification
                     BackgroundJobCoordinator.schedule<SyncLendingBackgroundJob>(
                         input = mapOf(SyncLendingBackgroundJobLogic.EXTRA_LENDING_ID to notification.lendingId.toString()),
