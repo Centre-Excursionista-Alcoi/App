a98af971765f9d2d7e9644b3cf0566312057289f
Arnau Mora <arnyminerz@proton.me>
2025-10-28 20:30:53 +0100

    Fix cast
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/permissionsMain/kotlin/org/centrexcursionistalcoi/app/permission/HelperHolder.permissions.kt
+++ composeApp/src/permissionsMain/kotlin/org/centrexcursionistalcoi/app/permission/HelperHolder.permissions.kt
@@ -3,5 +3,5 @@ package org.centrexcursionistalcoi.app.permission
 import tech.kotlinlang.permission.HelperHolder as LibHelperHolder
 
 actual object HelperHolder {
-    actual fun getPermissionHelperInstance(): PermissionHelper = LibHelperHolder.getPermissionHelperInstance()
+    actual fun getPermissionHelperInstance(): PermissionHelper = LibHelperHolder.getPermissionHelperInstance().asAppPermissionHelper()
 }
