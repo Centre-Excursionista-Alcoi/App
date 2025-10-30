97d707ad168a2b9c4665ee97c4881449c8b4bbfd
Arnau Mora <arnyminerz@proton.me>
2025-10-30 19:50:06 +0100

    Fix extension
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/main/kotlin/org/centrexcursionistalcoi/app/database/entity/SpaceEntity.kt
+++ server/src/main/kotlin/org/centrexcursionistalcoi/app/database/entity/SpaceEntity.kt
@@ -1,17 +1,20 @@
 package org.centrexcursionistalcoi.app.database.entity
 
+import java.time.Duration
 import java.util.UUID
 import kotlin.time.toKotlinDuration
 import kotlin.uuid.Uuid
 import kotlin.uuid.toKotlinUuid
 import org.centrexcursionistalcoi.app.data.Space
+import org.centrexcursionistalcoi.app.database.base.EntityPatcher
 import org.centrexcursionistalcoi.app.database.table.Spaces
+import org.centrexcursionistalcoi.app.request.UpdateSpaceRequest
 import org.jetbrains.exposed.v1.core.dao.id.EntityID
 import org.jetbrains.exposed.v1.dao.UUIDEntity
 import org.jetbrains.exposed.v1.dao.UUIDEntityClass
 import org.jetbrains.exposed.v1.jdbc.JdbcTransaction
 
-class SpaceEntity(id: EntityID<UUID>) : UUIDEntity(id), EntityDataConverter<Space, Uuid> {
+class SpaceEntity(id: EntityID<UUID>) : UUIDEntity(id), EntityDataConverter<Space, Uuid>, EntityPatcher<UpdateSpaceRequest> {
     companion object : UUIDEntityClass<SpaceEntity>(Spaces)
 
     var name by Spaces.name
@@ -34,4 +37,13 @@ class SpaceEntity(id: EntityID<UUID>) : UUIDEntity(id), EntityDataConverter<Spac
         },
         capacity = capacity,
     )
+
+    context(_: JdbcTransaction)
+    override fun patch(request: UpdateSpaceRequest) {
+        request.name?.let { name = it }
+        request.description?.let { description = it }
+        request.price?.let { price = it.toBigDecimal() }
+        request.priceDurationSeconds?.let { priceDuration = Duration.ofSeconds(it) }
+        request.capacity?.let { capacity = it }
+    }
 }
