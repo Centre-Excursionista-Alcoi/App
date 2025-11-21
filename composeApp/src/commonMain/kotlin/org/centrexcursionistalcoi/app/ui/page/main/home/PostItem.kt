546b33e5f65c537b09075db907c3a90c50077747
Arnau Mora <arnyminerz@proton.me>
2025-11-21 13:02:03 +0100

    Add zoom to post image
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/ui/page/main/home/PostItem.kt
+++ composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/ui/page/main/home/PostItem.kt
@@ -1,10 +1,13 @@
 package org.centrexcursionistalcoi.app.ui.page.main.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,9 +15,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Link
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedCard
@@ -27,17 +32,25 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.zIndex
 import cea_app.composeapp.generated.resources.*
+import coil3.compose.rememberAsyncImagePainter
 import com.mikepenz.markdown.coil3.Coil3ImageTransformerImpl
 import com.mikepenz.markdown.m3.Markdown
+import net.engawapg.lib.zoomable.rememberZoomState
+import net.engawapg.lib.zoomable.zoomable
 import org.centrexcursionistalcoi.app.data.ReferencedPost
 import org.centrexcursionistalcoi.app.data.localizedDate
 import org.centrexcursionistalcoi.app.data.rememberImageFiles
 import org.centrexcursionistalcoi.app.ui.reusable.AsyncByteImage
+import org.centrexcursionistalcoi.app.utils.isNullOrEmpty
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -94,9 +107,44 @@ fun PostItem(post: ReferencedPost) {
                 ) {
                     item("start_spacer") { Spacer(Modifier.width(12.dp)) }
                     items(images.toList()) { (_, image) ->
+                        var showingDialog by remember { mutableStateOf(false) }
+                        if (showingDialog) {
+                            Dialog(
+                                onDismissRequest = { showingDialog = false },
+                                properties = DialogProperties(
+                                    usePlatformDefaultWidth = false
+                                )
+                            ) {
+                                val painter = rememberAsyncImagePainter(image)
+                                val zoomState = rememberZoomState(contentSize = painter.intrinsicSize)
+                                Box(modifier = Modifier.fillMaxSize()) {
+                                    IconButton(
+                                        modifier = Modifier.align(Alignment.TopStart).padding(8.dp).zIndex(1f),
+                                        onClick = { showingDialog = false },
+                                    ) {
+                                        Icon(
+                                            imageVector = Icons.Default.Close,
+                                            contentDescription = stringResource(Res.string.close),
+                                            tint = Color.White
+                                        )
+                                    }
+
+                                    Image(
+                                        painter = painter,
+                                        contentDescription = null,
+                                        contentScale = ContentScale.Fit,
+                                        modifier = Modifier.fillMaxSize().zoomable(zoomState)
+                                    )
+                                }
+                            }
+                        }
+
                         AsyncByteImage(
                             bytes = image,
-                            modifier = Modifier.fillMaxHeight().padding(horizontal = 8.dp),
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .padding(horizontal = 8.dp)
+                                .clickable(enabled = !image.isNullOrEmpty()) { showingDialog = true },
                         )
                     }
                     item("end_spacer") { Spacer(Modifier.width(12.dp)) }
