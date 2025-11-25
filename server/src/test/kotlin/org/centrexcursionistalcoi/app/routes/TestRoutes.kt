4e714609dea80085b91d276aedab7dd942d65e6b
Arnau Mora <arnyminerz@proton.me>
2025-11-25 08:11:36 +0100

    Only provide inventory for member departments (#282)
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/test/kotlin/org/centrexcursionistalcoi/app/routes/TestRoutes.kt
+++ server/src/test/kotlin/org/centrexcursionistalcoi/app/routes/TestRoutes.kt
@@ -17,6 +17,7 @@ import org.centrexcursionistalcoi.app.database.entity.InventoryItemTypeEntity
 import org.centrexcursionistalcoi.app.database.entity.PostEntity
 import org.centrexcursionistalcoi.app.database.table.DepartmentMembers
 import org.centrexcursionistalcoi.app.routes.ProvidedRouteTests.runTestsOnRoute
+import org.centrexcursionistalcoi.app.test.*
 import org.centrexcursionistalcoi.app.utils.Zero
 import org.centrexcursionistalcoi.app.utils.toUUID
 import org.jetbrains.exposed.v1.jdbc.insert
