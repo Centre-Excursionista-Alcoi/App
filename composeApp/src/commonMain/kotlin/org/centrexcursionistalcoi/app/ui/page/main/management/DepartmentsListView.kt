b7fed5f390ecf0b003af796fe7934adf0b9c773d
Arnau Mora <arnyminerz@proton.me>
2026-01-19 19:13:30 +0100

    Display pending requests badge
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/ui/page/main/management/DepartmentsListView.kt
+++ composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/ui/page/main/management/DepartmentsListView.kt
@@ -1,15 +1,29 @@
 package org.centrexcursionistalcoi.app.ui.page.main.management
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Badge
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import cea_app.composeapp.generated.resources.*
 import io.github.vinceglb.filekit.PlatformFile
+import kotlin.uuid.Uuid
 import kotlinx.coroutines.Job
 import org.centrexcursionistalcoi.app.data.Department
 import org.centrexcursionistalcoi.app.data.DepartmentMemberInfo
@@ -27,7 +41,6 @@ import org.centrexcursionistalcoi.app.ui.reusable.buttons.TooltipIconButton
 import org.centrexcursionistalcoi.app.ui.reusable.form.FormImagePicker
 import org.centrexcursionistalcoi.app.ui.reusable.form.ReadOnlyFormField
 import org.jetbrains.compose.resources.stringResource
-import kotlin.uuid.Uuid
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -64,6 +77,12 @@ fun DepartmentsListView(
                 contentDescription = department.displayName,
                 modifier = Modifier.size(48.dp).clip(RoundedCornerShape(8.dp))
             )
+        },
+        itemTrailingContent = { department ->
+            val unconfirmedRequests = department.members?.count { !it.confirmed } ?: 0
+            if (unconfirmedRequests > 0) {
+                Badge { Text(unconfirmedRequests.toString()) }
+            }
         },
         emptyItemsText = stringResource(Res.string.management_no_departments),
         itemToolbarActions = {
