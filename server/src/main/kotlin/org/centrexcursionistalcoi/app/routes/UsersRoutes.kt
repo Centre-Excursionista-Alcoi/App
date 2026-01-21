34560af55cc807d68ef25eafc4ad95f341f0c2b9
Arnau Mora <arnyminerz@proton.me>
2026-01-21 09:51:09 +0100

    Ensure data compartimentation
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/main/kotlin/org/centrexcursionistalcoi/app/routes/UsersRoutes.kt
+++ server/src/main/kotlin/org/centrexcursionistalcoi/app/routes/UsersRoutes.kt
@@ -14,6 +14,8 @@ import org.centrexcursionistalcoi.app.database.entity.MemberEntity
 import org.centrexcursionistalcoi.app.database.entity.UserInsuranceEntity
 import org.centrexcursionistalcoi.app.database.entity.UserReferenceEntity
 import org.centrexcursionistalcoi.app.database.table.DepartmentMembers
+import org.centrexcursionistalcoi.app.database.table.LendingUsers
+import org.centrexcursionistalcoi.app.database.table.UserInsurances
 import org.centrexcursionistalcoi.app.database.table.UserReferences
 import org.centrexcursionistalcoi.app.error.Error
 import org.centrexcursionistalcoi.app.error.respondError
@@ -21,6 +23,7 @@ import org.centrexcursionistalcoi.app.plugins.UserSession.Companion.assertAdmin
 import org.centrexcursionistalcoi.app.plugins.UserSession.Companion.getUserSessionOrFail
 import org.jetbrains.exposed.v1.core.and
 import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.core.inList
 
 fun Route.usersRoutes() {
     // Provides a list of all users. Admins get all users, non-admins only get users in departments they manage (if they
@@ -51,25 +54,61 @@ fun Route.usersRoutes() {
             return@get
         }
 
-        val departmentMembers = Database { DepartmentMemberEntity.all().map { it.toData() } }
-        val lendingUsers = Database { LendingUserEntity.all().map { it.toData() } }
-        val insurances = Database { UserInsuranceEntity.all().map { it.toData() } }
         val users = Database {
             if (managingDepartments == null) {
                 // If admin, get all users
+                val departmentMembers = Database { DepartmentMemberEntity.all().map { it.toData() } }
+                val lendingUsers = Database { LendingUserEntity.all().map { it.toData() } }
+                val insurances = Database { UserInsuranceEntity.all().map { it.toData() } }
+
                 UserReferenceEntity.all()
+                    // Avoid duplicates in case a user is in multiple departments
+                    .toSet()
+                    // Map to data class including lending user and insurances
+                    .map { user ->
+                        user.toData(
+                            lendingUser = lendingUsers.find { it.sub == user.sub.value },
+                            insurances = insurances.filter { it.userSub == user.sub.value },
+                            departments = departmentMembers.filter { it.userSub == user.sub.value }
+                        )
+                    }
             } else {
                 // Else, get only users in the departments they manage
                 // Since if reached this point the user is at least inside a department, we are sure that themself is included, so no need to check
-                managingDepartments
+                val userReferences = managingDepartments
                     .flatMap { it.confirmedMembers }
                     .map { it.userReference }
-            }.toSet().map { user ->
-                user.toData(
-                    lendingUser = lendingUsers.find { it.sub == user.sub.value },
-                    insurances = insurances.filter { it.userSub == user.sub.value },
-                    departments = departmentMembers.filter { it.userSub == user.sub.value }
-                )
+                    .toSet()
+                val userSubs = userReferences.map { it.sub.value }
+
+                val departmentMembers = Database {
+                    DepartmentMemberEntity
+                        // Find members in the relevant departments
+                        .find { DepartmentMembers.userSub inList userSubs }
+                        .map { it.toData() }
+                }
+                val lendingUsers = Database {
+                    LendingUserEntity
+                        // Find members in the relevant departments
+                        .find { LendingUsers.userSub inList userSubs }
+                        .map { it.toData() }
+                }
+                val insurances = Database {
+                    UserInsuranceEntity
+                        // Find members in the relevant departments
+                        .find { UserInsurances.userSub inList userSubs }
+                        .map { it.toData() }
+                }
+
+                userReferences
+                    // Map to data class including lending user and insurances
+                    .map { user ->
+                        user.toData(
+                            lendingUser = lendingUsers.find { it.sub == user.sub.value },
+                            insurances = insurances.filter { it.userSub == user.sub.value },
+                            departments = departmentMembers.filter { it.userSub == user.sub.value }
+                        )
+                    }
             }
         }
 
