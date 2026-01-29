6c83f599fd123a1036397b429e2ff720d3a9c639
Arnau Mora Gras <arnyminerz@proton.me>
2026-01-29 08:08:22 +0100

    Allow scroll in feed item modal dialog
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/ui/page/main/home/FeedItem.kt
+++ composeApp/src/commonMain/kotlin/org/centrexcursionistalcoi/app/ui/page/main/home/FeedItem.kt
@@ -1,8 +1,25 @@
 package org.centrexcursionistalcoi.app.ui.page.main.home
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -20,10 +37,17 @@ fun FeedItem(
 ) {
     var showingDialog by remember { mutableStateOf(false) }
     if (showingDialog) {
+        val sheetState = rememberModalBottomSheetState()
+
         ModalBottomSheet(
+            sheetState = sheetState,
             onDismissRequest = { showingDialog = false },
         ) {
-            Column(Modifier.padding(8.dp)) {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .verticalScroll(state = rememberScrollState(), enabled = sheetState.currentValue == SheetValue.Expanded)
+            ) {
                 Text(
                     text = title,
                     style = MaterialTheme.typography.titleLarge,
