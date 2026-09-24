26d323c56aa929444b18da6c30d4418824a4c457
Arnau Mora Gras <arnyminerz@proton.me>
2026-09-24 09:17:42 +0200

    Disallow all
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- server/src/main/kotlin/org/centrexcursionistalcoi/app/routes/RobotsRoute.kt
+++ server/src/main/kotlin/org/centrexcursionistalcoi/app/routes/RobotsRoute.kt
@@ -6,11 +6,11 @@ import io.ktor.server.routing.get
 
 fun Route.robotsRoute() {
     get("/robots.txt") {
-        // Disallow all crawlers from downloading files
+        // Disallow all crawlers from indexing this server, because it contains sensitive data (user accounts, events, etc.) and is not meant to be public.
         call.respondText(
             """
                 User-agent: *
-                Disallow: /download/
+                Disallow: /
             """.trimIndent()
         )
     }
